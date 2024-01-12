a4bde547804ea73437081d6747a5ad0f21b00573
ᴬᵈᵃᵐ <61629560+iyalex250@users.noreply.github.com>
2024-01-12 10:52:18 +0200

    Iterator.java

---
+++ Iterator.java
@@ -0,0 +1,9 @@
+//@iyalex
+
+public interface Iterator {
+	//This is the interface for the Iterator pattern.
+		boolean hasNext(int position);
+		Object next(int position);
+		boolean hasPrev(int position);
+	
+}
